83ccaf7637163663334d0827c6a7b2f0c51dc4a1
JCastroUnreal <JCastroUnreal@gmail.com>
2024-07-30 16:39:01 +0200

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using WPF_JCastro_TaskManager.Views;
+
+namespace WPF_JCastro_TaskManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ViewTasks_Click(object sender, RoutedEventArgs e)
+        {
+            MainContentControl.Content = new ViewTasks();
+        }
+
+        private void AddTasks_Click(object sender, RoutedEventArgs e)
+        {
+            MainContentControl.Content = new AddTasks();
+        }
+
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+            MainContentControl.Content = new Settings();
+        }
+    }
+}
